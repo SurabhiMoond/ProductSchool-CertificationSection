017406ff2c61c155398da6af7f3829ef2755c2f8
Surabhi Moond <154314470+SurabhiMoond@users.noreply.github.com>
2024-05-13 14:27:45 +0530

    Update App.tsx

--- product-school/src/App.tsx
+++ product-school/src/App.tsx
@@ -1,16 +1,3 @@
-// import "./App.css";
-// import { RoutingCertificate } from "./routesCertificate/routingCertificate";
-
-// const App = () => {
-//   return (
-//     <div>
-//       <RoutingCertificate />
-//     </div>
-//   );
-// };
-
-// export default App;
-// App.jsx
 import "./App.css";
 import { EnrollmentProvider } from "./context/EnrollmentProvider";
 import { RoutingCertificate } from "./routesCertificate/routingCertificate";
